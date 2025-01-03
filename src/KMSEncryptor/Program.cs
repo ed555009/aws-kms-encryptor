49cdd4dc23ba84d6fc61ca50dbdda3f24c392423
Ed Wang <ed555009@gmail.com>
2025-01-03 18:57:16 +0800

    Add initial project structure and implementation for AWS KMS encryptor

---
+++ src/KMSEncryptor/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using Amazon.KeyManagementService;
+using Amazon.KeyManagementService.Model;
+
+Console.WriteLine("Enter KMS Key ID: ");
+
+var keyId = Console.ReadLine();
+
+Console.WriteLine("Enter Data to encrypt: ");
+
+List<string> lines = [];
+string? input;
+
+while (true)
+{
+	input = Console.ReadLine();
+
+	if (string.IsNullOrWhiteSpace(input))
+		break;
+
+	lines.Add(input);
+}
+
+string data = string.Join("\n", lines);
+
+try
+{
+	using var client = new AmazonKeyManagementServiceClient();
+	var result = await client.EncryptAsync(new EncryptRequest
+	{
+		KeyId = keyId,
+		Plaintext = new MemoryStream(Encoding.UTF8.GetBytes(data)),
+		EncryptionAlgorithm = EncryptionAlgorithmSpec.SYMMETRIC_DEFAULT
+	});
+
+	if (result == null)
+	{
+		Console.WriteLine("Failed to encrypt data");
+		Environment.Exit(1);
+	}
+
+	Console.WriteLine("Encrypted Data:");
+	Console.WriteLine(Convert.ToBase64String(result.CiphertextBlob.ToArray()));
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"Failed to encrypt data: {ex.Message}");
+}
+finally
+{
+	Environment.Exit(0);
+}
